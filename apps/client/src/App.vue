69b132de2f1c83b928d22e9605a8cae5d2cdebf5
Rassul <rasul.tabulov@gmail.com>
2022-03-11 12:01:40 +0600

    init

---
+++ apps/client/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from 'vue';
+
+const users = ref([]);
+fetch('/api/users')
+  .then((r) => r.json())
+  .then((usersResponse) => (users.value = usersResponse));
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <pre :style="{ textAlign: 'left' }"
+    >{{ JSON.stringify(users, null, 2) }}
+  </pre>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
