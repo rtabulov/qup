78c20d10b58530dfe8c9685f8f4add95fa1af66e
Rassul <rasul.tabulov@gmail.com>
2022-03-16 15:44:25 +0600

    improve seeds

--- apps/server/src/seeds/create-departments.seed.ts
+++ apps/server/src/seeds/create-departments.seed.ts
@@ -1,5 +1,6 @@
 import { Connection } from 'typeorm';
 import { Department } from '../departments/entities/department.entity';
+import { Teacher } from '../teachers/entities/teacher.entity';
 
 const DEPARTMENTS_SEED = [
   { name: 'Кафедра "Бизнес и управление"' },
@@ -12,10 +13,29 @@ const DEPARTMENTS_SEED = [
   { name: 'Факультет экономики и сервиса' },
 ];
 
+const TEACHERS_SEED = [
+  {
+    firstName: 'Пётр',
+    lastName: 'Петров',
+    middleName: 'Петрович',
+    email: 'petr@gmail.com',
+    password: 'wordpass',
+    position: 'Старший преподаватель',
+  },
+  {
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    middleName: 'Иванович',
+    email: 'ivan@gmail.com',
+    password: 'wordpass',
+    position: 'Старший преподаватель',
+  },
+];
+
 export default class CreateDepartments {
   async run(factory, connection: Connection) {
-    const all = await connection.getRepository(Department).find();
-    await connection.getRepository(Department).remove(all);
+    await connection.createQueryBuilder().delete().from(Teacher).execute();
+    await connection.createQueryBuilder().delete().from(Department).execute();
 
     await connection
       .createQueryBuilder()
@@ -24,5 +44,19 @@ export default class CreateDepartments {
       .values(DEPARTMENTS_SEED)
       .orIgnore()
       .execute();
+
+    const departments = await connection.getRepository(Department).find();
+
+    const seed = TEACHERS_SEED.map((t) => {
+      const rand = Math.floor(Math.random() * departments.length);
+      return { ...t, department: departments[rand].id };
+    });
+
+    await Promise.all(
+      seed.map((teacher) => {
+        const t = connection.getRepository(Teacher).create(teacher);
+        return connection.getRepository(Teacher).save(t);
+      }),
+    );
   }
 }
