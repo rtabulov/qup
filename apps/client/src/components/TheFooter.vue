36b1f3e002ff7ecbc813f17798039f0693e6c47d
Rassul <rasul.tabulov@gmail.com>
2022-03-23 15:00:28 +0600

    update footer links

--- apps/client/src/components/TheFooter.vue
+++ apps/client/src/components/TheFooter.vue
@@ -1,32 +1,102 @@
 <script lang="ts" setup>
 import { getRandomEmoji } from '../utils/getRandomEmoji';
+import AppRuler from './AppRuler.vue';
+import InlineLink from './InlineLink.vue';
 </script>
 
 <template>
   <footer
     class="bg-black text-white text-opacity-75 py-4 space-y-4 font-display"
   >
-    <nav class="grid grid-cols-5 container">
+    <h2 class="container text-xl text-white">
+      Список использованых инструментов
+    </h2>
+
+    <nav class="grid grid-cols-4 gap-6 container">
       <div class="flex flex-col">
-        <a role="link" aria-disabled="true">Lorem</a>
-        <a role="link" aria-disabled="true">ipsum</a>
-        <a role="link" aria-disabled="true">dolor,</a>
-        <a role="link" aria-disabled="true">sit</a>
-        <a role="link" aria-disabled="true">amet</a>
+        <h3 class="text-white font-semibold text-lg">Common</h3>
+        <AppRuler class="w-24 !my-2" />
+        <div>
+          <InlineLink
+            class="text-opacity-90"
+            to="https://www.typescriptlang.org/"
+            >TypeScript</InlineLink
+          >
+        </div>
+        <div>
+          <InlineLink class="text-opacity-90" to="https://pnpm.io/"
+            >pnpm</InlineLink
+          >
+          as monorepo & dependency manager
+        </div>
       </div>
       <div class="flex flex-col">
-        <a role="link" aria-disabled="true">consectetur</a>
-        <a role="link" aria-disabled="true">adipisicing</a>
-        <a role="link" aria-disabled="true">elit.</a>
-        <a role="link" aria-disabled="true">Ducimus</a>
-        <a role="link" aria-disabled="true">nesciunt</a>
+        <h3 class="text-white font-semibold text-lg">Backend</h3>
+        <AppRuler class="w-24 !my-2" />
+        <div>
+          <InlineLink class="text-opacity-90" to="https://nestjs.com/"
+            >NestJS</InlineLink
+          >
+          framework
+        </div>
+        <div>
+          <InlineLink class="text-opacity-90" to="https://www.passportjs.org/"
+            >passport + passport-local</InlineLink
+          >
+          for authentication
+        </div>
+        <div>
+          <InlineLink
+            class="text-opacity-90"
+            to="https://github.com/exceljs/exceljs"
+            >exceljs</InlineLink
+          >
+          as report generator
+        </div>
       </div>
       <div class="flex flex-col">
-        <a role="link" aria-disabled="true">sint</a>
-        <a role="link" aria-disabled="true">deserunt</a>
-        <a role="link" aria-disabled="true">natus</a>
-        <a role="link" aria-disabled="true">alias</a>
-        <a role="link" aria-disabled="true">aspernatur?</a>
+        <h3 class="text-white font-semibold text-lg">Database</h3>
+        <AppRuler class="w-24 !my-2" />
+        <div>
+          <InlineLink class="text-opacity-90" to="https://www.mysql.com/"
+            >MySQL</InlineLink
+          >
+          as primary database
+        </div>
+        <div>
+          <InlineLink class="text-opacity-90" to="https://typeorm.io/#/"
+            >TypeORM</InlineLink
+          >
+          as ORM layer
+        </div>
+        <div>
+          <InlineLink class="text-opacity-90" to="https://redis.io/"
+            >Redis</InlineLink
+          >
+          as session store
+        </div>
+      </div>
+      <div class="flex flex-col">
+        <h3 class="text-white font-semibold text-lg">Frontend</h3>
+        <AppRuler class="w-24 !my-2" />
+        <div>
+          <InlineLink class="text-opacity-90" to="https://vuejs.org/"
+            >Vue</InlineLink
+          >
+          framework
+        </div>
+        <div>
+          <InlineLink class="text-opacity-90" to="https://tailwindcss.com/"
+            >Tailwind</InlineLink
+          >
+          for styling
+        </div>
+        <div>
+          <InlineLink class="text-opacity-90" to="https://vitejs.dev/"
+            >Vite</InlineLink
+          >
+          frontend tooling
+        </div>
       </div>
     </nav>
 
