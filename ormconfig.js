f3a6a342d7866fc6304b0f56f902ba188c63a5ce
Rassul <rasul.tabulov@gmail.com>
2022-03-24 15:05:16 +0600

    try to fix seed on server

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,9 @@
+console.log(process.env.DATABASE_URL);
+console.log(process.env.DATABASE_URL);
+console.log(process.env.DATABASE_URL);
+
 module.exports = {
+  url: process.env.DATABASE_URL,
   host: 'localhost',
   port: 5432,
   username: 'postgres',
