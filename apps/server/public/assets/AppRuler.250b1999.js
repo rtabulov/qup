1bc822d0cc83fa6f6f5717ac7f93caae259a70ea
Rassul <rasul.tabulov@gmail.com>
2022-04-01 00:38:44 +0600

    cleanup public folder

--- apps/server/public/assets/AppRuler.250b1999.js
+++
@@ -1,1 +0,0 @@
-import{b as r,o,g as a,A as s}from"./vendor.8a8b2f57.js";const l=r({props:{center:{type:Boolean,default:!1}},setup(e){return(t,n)=>(o(),a("hr",{class:s(["max-w-xs my-6 border-red border-b-[3px]",{"mx-auto":e.center}])},null,2))}});export{l as _};
--- apps/server/public/server/assets/_default.page.client.1eb26561.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-throw new Error("[vite-plugin-ssr][Wrong Usage] File /home/rassul/qup/apps/client/src/pages/_default.page.client.ts should not be loaded in Node.js");
