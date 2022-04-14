c39d647eac4cf3e4df085838922dc89e7eb960f6
Rassul <rasul.tabulov@gmail.com>
2022-04-14 11:04:36 +0600

    add validation to auth models

--- apps/server/src/auth/models/register-user.dto.ts
+++ apps/server/src/auth/models/register-user.dto.ts
@@ -1,10 +1,29 @@
+import { IsEmail, IsString, MinLength, IsUUID } from 'class-validator';
+
 export class RegisterUserDto {
-  confirmationPassword: string;
+  @IsString()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsString()
   middleName: string;
+
+  @IsUUID()
   departmentId: string;
+
+  @IsString()
   position: string;
+
+  @IsEmail()
+  @MinLength(6)
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
+
+  @IsString()
+  confirmationPassword: string;
 }
--- apps/server/src/auth/models/update-user.dto.ts
+++ apps/server/src/auth/models/update-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsBoolean, IsOptional, IsUUID } from 'class-validator';
+
 export class UpdateUserDto {
+  @IsUUID()
+  @IsOptional()
   roleId?: string;
+
+  @IsBoolean()
+  @IsOptional()
   active?: boolean;
 }
