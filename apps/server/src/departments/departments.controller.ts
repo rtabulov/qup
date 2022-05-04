47324333c95a0d69e05f68630b389ec72112693d
Rassul <rasul.tabulov@gmail.com>
2022-05-04 12:29:41 +0600

    remove unnecessary comments

--- apps/server/src/departments/departments.controller.ts
+++ apps/server/src/departments/departments.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { DepartmentsService } from './departments.service';
 
 @Controller('departments')
--- apps/server/src/supabase/supabase.service.ts
+++ apps/server/src/supabase/supabase.service.ts
@@ -1,15 +1,7 @@
-import {
-  INestApplication,
-  Injectable,
-  OnModuleDestroy,
-  OnModuleInit,
-} from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { SupabaseClient } from '@supabase/supabase-js';
 
-/**
- * Extension of the PrismaClient for use with NestJs.
- */
 @Injectable()
 export class SupabaseService extends SupabaseClient implements OnModuleInit {
   constructor(private readonly configService: ConfigService) {
@@ -29,30 +21,4 @@ export class SupabaseService extends SupabaseClient implements OnModuleInit {
       throw new Error('could not sign in to supabase');
     }
   }
-
-  /**
-   * Disconnect from the database when the application is shutting down.
-   */
-  // async onModuleDestroy(): Promise<void> {
-  //   await this.$disconnect();
-  // }
-
-  /**
-   * A utility function used to clear all database rows for testing.
-   */
-  // clearDatabase() {
-  //   const modelNames = Prisma.dmmf.datamodel.models.map((model) => model.name);
-
-  //   return Promise.all(
-  //     modelNames.map((modelName) =>
-  //       this[modelName[0].toLowerCase() + modelName.slice(1)].deleteMany(),
-  //     ),
-  //   );
-  // }
-
-  // async enableShutdownHooks(app: INestApplication) {
-  //   this.$on('beforeExit', async () => {
-  //     await app.close();
-  //   });
-  // }
 }
