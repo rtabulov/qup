abaf96751b34fa87b15115d6f0fbaf8fe7994994
Rassul <rasul.tabulov@gmail.com>
2022-05-19 16:39:15 +0600

    refactor

--- apps/server/src/certificates/certificates.service.ts
+++ apps/server/src/certificates/certificates.service.ts
@@ -5,114 +5,44 @@ import {
   BadRequestException,
   ForbiddenException,
 } from '@nestjs/common';
-import { groupBy, sortBy } from 'lodash';
-import * as ExcelJS from 'exceljs';
-import { ConfigService } from '@nestjs/config';
 import { User, Role, Prisma } from '@prisma/client';
-
 import { UpdateCertificateDto } from './dto/update-certificate.dto';
 import { CreateCertificateDto } from './dto/create-certificate.dto';
 import { resolveAuthLevel } from '../utils';
 import { PrismaService } from '../prisma';
-
+import { genereateReport } from '../utils/generateReport';
 @Injectable()
 export class CertificatesService {
-  constructor(
-    private readonly prismaService: PrismaService,
-    private readonly configService: ConfigService,
-  ) {}
-
-  create(
-    certificate: CreateCertificateDto & {
-      teacherId: string;
-    },
-  ) {
-    return this.prismaService.certificate.create({
-      data: { ...certificate },
-    });
+  constructor(private readonly prismaService: PrismaService) {}
+  create(certificate: CreateCertificateDto & { teacherId: string }) {
+    return this.prismaService.certificate.create({ data: { ...certificate } });
   }
-
   findAll() {
     return this.prismaService.certificate.findMany({
       include: { teacher: true, files: true },
     });
   }
-
   findApproved() {
     return this.prismaService.certificate.findMany({
       where: { approved: true },
       include: { teacher: { include: { department: true } }, files: true },
     });
   }
-
   findAwaitingApproval() {
     return this.prismaService.certificate.findMany({
       where: { approved: false, awaitingApproval: true },
       include: { teacher: true, files: true },
     });
   }
-
   async exportAll() {
-    const certificates = (await this.findApproved()).map((c) => ({
-      ...c,
-      teacher: {
-        ...c.teacher,
-        fullName: `${c.teacher.lastName} ${c.teacher.firstName} ${c.teacher.middleName}`,
-      },
-    }));
-
-    const groupedByDepartments = groupBy(
-      certificates,
-      'teacher.department.name',
-    );
-
-    const workbook = new ExcelJS.Workbook();
-    const host = this.configService.get('HOST');
-
-    Object.entries(groupedByDepartments).forEach(
-      ([department, certificates]) => {
-        const ws = workbook.addWorksheet(department);
-        ws.columns = [
-          // { header: 'ID', key: 'id' },
-          { header: 'ФИО преподавателя', key: 'fullName' },
-          { header: 'Название курса', key: 'name' },
-          { header: 'Кем выдан', key: 'issuedBy' },
-          { header: 'Дата выдачи', key: 'issuedDate' },
-          { header: 'Дата начала обучения', key: 'startDate' },
-          { header: 'Дата окончания обучения', key: 'endDate' },
-          { header: 'Прикрепленные файлы', key: 'files' },
-        ];
-        ws.addRows(
-          sortBy(certificates, 'teacher.fullName').map((cert) => ({
-            // id: cert.id,
-            fullName: cert.teacher.fullName,
-            name: cert.name,
-            issuedBy: cert.issuedBy,
-            issuedDate: new Date(cert.issuedDate),
-            startDate: new Date(cert.startDate),
-            endDate: new Date(cert.endDate),
-            files: cert.files
-              .map((f) => `${host}/api/uploads/certificates/${f.name}`)
-              .join('\n'),
-          })),
-        );
-
-        // autoHeight(ws);
-        autoWidth(ws);
-      },
-    );
-
-    return workbook;
+    const certificates = await this.findApproved();
+    return genereateReport(certificates);
   }
-
   findOne(id: string, { expand = false } = {}) {
     try {
       return this.prismaService.certificate.findUnique({
         where: { id },
-        include: {
-          teacher: expand,
-          files: expand,
-        },
+        include: { teacher: expand, files: expand },
       });
     } catch (e) {
       if (e instanceof Prisma.PrismaClientValidationError) {
@@ -120,7 +50,6 @@ export class CertificatesService {
       }
     }
   }
-
   async update(
     id: string,
     user: User & { role: Role },
@@ -130,18 +59,15 @@ export class CertificatesService {
     if (!found) {
       throw new NotFoundException();
     }
-
     if (found.approved) {
       throw new BadRequestException();
     }
-
     if (
       found.teacher.id !== user.id &&
       !resolveAuthLevel(user.role.key, 'hr')
     ) {
       throw new ForbiddenException();
     }
-
     return this.prismaService.certificate.update({
       where: { id },
       data: {
@@ -150,36 +76,14 @@ export class CertificatesService {
       },
     });
   }
-
   async remove(id: string, requestUser: User) {
     const cert = await this.findOne(id, { expand: true });
-
     if (requestUser.id !== cert.teacher.id) {
       throw new UnauthorizedException();
     }
-
     if (cert.approved) {
       throw new UnauthorizedException();
     }
-
     return this.prismaService.certificate.delete({ where: { id } });
   }
 }
-
-const autoWidth = (worksheet: ExcelJS.Worksheet, minimalWidth = 10) => {
-  worksheet.columns.forEach((column) => {
-    let maxColumnLength = 0;
-    column.eachCell({ includeEmpty: true }, (cell) => {
-      maxColumnLength = Math.max(
-        maxColumnLength,
-        minimalWidth,
-        cell.value
-          ? cell.value instanceof Date
-            ? 8
-            : cell.value.toString().length
-          : 0,
-      );
-    });
-    column.width = maxColumnLength + 2;
-  });
-};
---
+++ apps/server/src/utils/generateReport.ts
@@ -0,0 +1,76 @@
+import * as ExcelJS from 'exceljs';
+import { groupBy, sortBy } from 'lodash';
+import { Certificate, FileMeta, User, Department } from '@prisma/client';
+
+const autoWidth = (worksheet: ExcelJS.Worksheet, minimalWidth = 10) => {
+  worksheet.columns.forEach((column) => {
+    let maxColumnLength = 0;
+    column.eachCell({ includeEmpty: true }, (cell) => {
+      maxColumnLength = Math.max(
+        maxColumnLength,
+        minimalWidth,
+        cell.value
+          ? cell.value instanceof Date
+            ? 8
+            : cell.value.toString().length
+          : 0,
+      );
+    });
+    column.width = maxColumnLength + 2;
+  });
+};
+
+export function genereateReport(
+  certificates: (Certificate & {
+    files: FileMeta[];
+    teacher: User & {
+      department: Department;
+    };
+  })[],
+) {
+  const groupedByDepartments = groupBy(certificates, 'teacher.department.name');
+
+  const workbook = new ExcelJS.Workbook();
+  const host = this.configService.get('HOST');
+
+  Object.entries(groupedByDepartments).forEach(([department, certificates]) => {
+    const ws = workbook.addWorksheet(department);
+    ws.columns = [
+      // { header: 'ID', key: 'id' },
+      { header: 'ФИО преподавателя', key: 'fullName' },
+      { header: 'Название курса', key: 'name' },
+      { header: 'Кем выдан', key: 'issuedBy' },
+      { header: 'Дата выдачи', key: 'issuedDate' },
+      { header: 'Дата начала обучения', key: 'startDate' },
+      { header: 'Дата окончания обучения', key: 'endDate' },
+      { header: 'Прикрепленные файлы', key: 'files' },
+    ];
+    ws.addRows(
+      sortBy(certificates, 'teacher.fullName')
+        .map((c) => ({
+          ...c,
+          teacher: {
+            ...c.teacher,
+            fullName: `${c.teacher.lastName} ${c.teacher.firstName} ${c.teacher.middleName}`,
+          },
+        }))
+        .map((cert) => ({
+          // id: cert.id,
+          fullName: cert.teacher.fullName,
+          name: cert.name,
+          issuedBy: cert.issuedBy,
+          issuedDate: new Date(cert.issuedDate),
+          startDate: new Date(cert.startDate),
+          endDate: new Date(cert.endDate),
+          files: cert.files
+            .map((f) => `${host}/api/uploads/certificates/${f.name}`)
+            .join('\n'),
+        })),
+    );
+
+    // autoHeight(ws);
+    autoWidth(ws);
+  });
+
+  return workbook;
+}
